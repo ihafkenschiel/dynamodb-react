412cedc21d134645e59d327682cce269d8b62202
Ian Hafkenschiel <ihafkenschiel@gmail.com>
2022-04-24 19:08:38 -0500

    React integrated with DynamoDB

---
+++ src/AwsFunctions.js
@@ -0,0 +1,29 @@
+import * as AWS from 'aws-sdk'
+
+console.log(
+  'process.env.REACT_APP_AWS_REGION :>> ',
+  process.env.REACT_APP_AWS_REGION
+)
+
+const configuration = {
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET,
+  region: 'us-west-1',
+}
+AWS.config.update(configuration)
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+export const fetchData = (tableName) => {
+  var params = {
+    TableName: tableName,
+  }
+
+  docClient.scan(params, function (err, data) {
+    if (!err) {
+      console.log(data)
+    } else {
+      console.error('Data not found', err)
+    }
+  })
+}
